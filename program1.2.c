8eb62c6df9f8f0ee6967750c4b9780ea592533ad
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-01 16:13:42 +0530

    Add files via upload

---
+++ program1.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	int c;
+	c=a*a-2*a*b+b*b;
+	printf("ans:%d",c);
+	
+}
+// output
+	A=5
+	B=10
+	ans:25
+--------------------------------
+//
