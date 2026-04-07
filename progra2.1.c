62d13733593f05a4cc1a3123bfeea92bc6d101b1
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-07 17:27:19 +0530

    Add files via upload

---
+++ progra2.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i=0,n;
+	printf("enter n");
+	scanf("%d",&n);
+	while(i<=n){
+		printf("%d\n",i);
+		i+=2;
+	}
+	
+}
+/* 
+output
+*/
---
+++ program2.2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int i,n;
+printf("enter n");
+scanf("%d",&n);
+while(i<=n){
+	printf("%d\n",i*i);
+	i++;
+}
+
+}
