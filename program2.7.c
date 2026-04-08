ddadfec92363f035be817f78e909d74377a89551
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-08 16:56:52 +0530

    Add files via upload

---
+++ program2.7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	for(ch='B' ; ch<='Y'; ch++){
+		if(ch%2!=0){
+			printf("%c ",ch);
+		}
+		
+	}
+}
+/* 
+output
+A C E G I K M O Q S U W Y
+--------------------------------
+*/
