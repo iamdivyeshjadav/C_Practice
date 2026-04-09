8221fb07ec35df4e2c70f970cbd40b468fbd1008
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-09 15:49:22 +0530

    Add files via upload

---
+++ program2.8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	i=1;
+	printf("enter nomber\n");
+	do{
+		printf("%d ",i);
+		i++;
+	}while(i<=10);
+}
+/* 
+output
+enter nomber
+1 2 3 4 5 6 7 8 9 10
+--------------------------------
+*/
---
+++ program2.9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	i=1;
+	printf("enter number\n");
+	for(i=10;i>=1;i--)
+	printf("%d ",i);
+}
+/*output
+enter number
+10 9 8 7 6 5 4 3 2 1
+--------------------------------
+*/
