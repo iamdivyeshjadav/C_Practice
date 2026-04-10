8af1002019d562f8e8c323cf5c4003d4eed205ab
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-10 18:43:06 +0530

    Add files via upload

---
+++ program3.3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=10;i++)
+	{
+      if(i==1||i==3||i==5||i==7){
+      	printf("* * * * * * * * * * ");
+	  }else{
+	  	printf("*        *        *  ");
+	  }
+	  printf("\n");
+	}
+	
+}
+/*
+output
+
+*/
---
+++ program3.4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=10;i++)
+	{
+      if(i==1){
+      	printf("*                   *");
+	  }else{
+	  	printf(" *                * ");
+	  	if(i==2){
+	  	printf(" *               *");
+		  }else{
+		printf("  *            *");
+		  }
+	  }
+	  printf("\n");
+	}
+		{
+      if(i==1){
+      	printf("*                   *");
+	  }else{
+	  	printf(" *                * ");
+	  	if(i==2){
+	  	printf(" *               *");
+		  }else{
+		printf("  *            *");
+		  }
+	  }
+	  printf("\n");
+	}
+}	
